78f00ab0347306affae9d935d58aaf862fe88b6c
Salvatore Nicosia <nicosia.salvo24@gmail.com>
2020-11-30 12:35:35 -0800

    Added more graphics objects and fixed splash screen and game over screen

--- projects/space_invaders/l5_application/game_graphics/game_graphics.h
+++ projects/space_invaders/l5_application/game_graphics/game_graphics.h
@@ -33,7 +33,13 @@
 void game_graphics__display_splash_screen(void);
 void game_graphics__display_game_over_screen(void);
 void game_graphics__display_laser_cannon(uint8_t row, uint8_t column, led_color_e color);
-void game_graphics__display_octopus(uint8_t row, uint8_t column, led_color_e color); // Large Invader
-void game_graphics__display_crab(uint8_t row, uint8_t column, led_color_e color);    // Medium Invader
-void game_graphics__display_squid(uint8_t row, uint8_t column, led_color_e color);   // Small Invader
+void game_graphics__display_octopus(uint8_t row, uint8_t column, led_color_e color);         // Large Invader
+void game_graphics__display_octopus_walking(uint8_t row, uint8_t column, led_color_e color); // Lager Invader Walking
+void game_graphics__display_crab(uint8_t row, uint8_t column, led_color_e color);            // Medium Invader
+void game_graphics__display_crab_walking(uint8_t row, uint8_t column, led_color_e color);    // Medium Invader Walking
+void game_graphics__display_squid(uint8_t row, uint8_t column, led_color_e color);           // Small Invader
+void game_graphics__display_squid_walking(uint8_t row, uint8_t column, led_color_e color);   // Small Invader Walking
 void game_graphics__display_ufo(uint8_t row, uint8_t column, led_color_e color);
+void game_graphics__display_enemy_bullet(uint8_t row, uint8_t column, led_color_e color);
+void game_graphics__display_laser_cannon_bullet(uint8_t row, uint8_t column, led_color_e color);
+void game_graphics__display_explotion(uint8_t row, uint8_t column, led_color_e color);
